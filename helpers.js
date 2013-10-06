e578beb1dd02374da39f99d4fd600f8c93c986d0
root <root@skanda-VirtualBox.(none)>
2013-10-06 06:29:35 -0400

    compare super revised

--- helpers.js
+++ helpers.js
@@ -1,85 +1,91 @@
 compare2 = function(prop1, prop2){
-	
+
 	var combinedInput1 = [];
 	var combinedInput2 = [];
 	
-	var cont = [];
-	var onlyp1 = [];
-	var onlyp2 = [];
 	var common = [];
+	var diff = [];
+	var uniq = [];
 	
+	//Grab the name-sentimentScore pair for each input keyword
+	$.each(prop1.keywords, function(k,keyword){
+		combinedInput1.push({'name':keyword.text.toLowerCase(),'score':parseFloat(keyword.sentiment.score)});
+	});
+	
+	//Grab the name-sentimentScore pair for each input entity, without name duplicates from keywords
 	$.each(prop1.entities, function(k,entity){
-	    combinedInput1.push({'name':entity.text.toLowerCase(),'score':entity.sentiment.score});
+        	add = true;
+        	$.each(combinedInput1, function(h,named){
+			if(entity.text.toLowerCase()==named.name){
+				add = false;
+			}
+		});
+        
+		if(add){
+            		combinedInput1.push({'name':entity.text.toLowerCase(),'score':parseFloat(entity.sentiment.score)});
+        	} 
 	});
 	
-	
-    $.each(prop1.keywords, function(k,keyword){
-        add = true;
-        for(i in combinedInput1){
-            if(keyword.text.toLowerCase()==combinedInput1[i].name){
-                add = false;
-            }
-        }
-        if(add){
-            combinedInput1.push({'name':keyword.text.toLowerCase(),'score':keyword.sentiment.score});
-        } 
+	//Likewise for the other input object
+
+	$.each(prop2.keywords, function(k,keyword){
+		combinedInput2.push({'name':keyword.text.toLowerCase(),'score':parseFloat(keyword.sentiment.score)});
 	});
-	
 	
 	$.each(prop2.entities, function(k,entity){
-	    combinedInput2.push({'name':entity.text.toLowerCase(),'score':entity.sentiment.score});
+		add = true;
+		$.each(combinedInput2, function(h, named){
+			if(entity.text.toLowerCase()==combinedInput2[i].name){
+				add = false;
+			}
+		}
+        	
+        	if(add){
+            	combinedInput2.push({'name':keyword.text.toLowerCase(),'score':parseFloat(keyword.sentiment.score)});
+        	}
 	});
 	
-    $.each(prop2.keywords, function(k,keyword){
-	    add = true;
-        for(i in combinedInput2){
-            if(keyword.text.toLowerCase()==combinedInput2[i].name){
-                add = false;
-            }
-        }
-        if(add){
-            combinedInput2.push({'name':keyword.text.toLowerCase(),'score':keyword.sentiment.score});
-        }
-	});
-	
+console.log("OUTPUT FOR STORED COMBINEDINPUT1 AND COMBINEDINPUT2");
+console.log(combinedInput1);
+console.log(combinedInput2);
 
-	for(i in combinedInput1){
-		var inOther = false;
-		for(j in combinedInput2){
-			if(combinedInput1[i].name==combinedInput1[j].name){
-				if(combinedInput1[i].score>0 && combinedInput1[j].score > 0){
-					common.push({'name':combinedInput1[i].name,'score':(parseFloat(combinedInput1[i].score)+parseFloat(combinedInput1[j].score))});
-				} else if (combinedInput1[i].score < 0 && combinedInput1[j].score < 0){
-					common.push({'name':combinedInput1[i].text,'score':(parseFloat(combinedInput1[i].score)+parseFloat(combinedInput1[j].score))});
+//Iterate over each pair of elements to detect common elements
+	$.each(combinedInput1, function(k, entry1){
+		
+		var uniq = true;
+		$.each(combinedInput2, function(h, entry2){
+			if(entry1.name==entry2.name){
+				if((entry1.score>0 && entry2.score>0)||(entry1.score<0 && entry2.score<0){
+					sum = entry1.score + entry2.score;
+					common.push({'name':entry1.name,'score',sum)});
 				} else {
-					cont.push({'name':combinedInput1[i].text,'score1':combinedInput1[i].score,'score2':combinedInput1[j].score});
+					diff.push({'name':entry1.name,'score1':entry1.score,'score2':entry2.score});
 				}
-				inOther = true;
+				uniq = false; 
+			}
+		});
+		if(uniq) {
+			uniq.push({'name':entry1.name, 'score':entry1.score});
+		}
+	});
+
+//Get unique elements from second input set
+	$.each(combinedInput2, function(k,entry1){
+		var uniq = true;
+		$.each(common, function(h,entry2){
+			if(entry1.name==entry2.name){
+				uniq = false;
 			}
 		}
-		
-		if(inOther == false){
-			onlyp1.push({'name':combinedInput1[i].text,'score1':combinedInput1[i].score});
+		if(uniq){
+			uniq.push({'name':entry1.name,'score':entry1.score});
 		}
 	}
-	
-	//janky but test cases are small
-	for(i in combinedInput2){
-		var inOther = false;
-		for(j in combinedInput1){
-			if(combinedInput1[i].name==combinedInput1[j].name){
-				inOther = true;
-			}
-		}
-		
-		if(inOther == false){
-			onlyp2.push({'name':combinedInput1[j].text,'score1':combinedInput1[j].score});
-		}
-	}
+
 	
 	return {
 	    "common": common,
-	    "unique": onlyp1.concat(onlyp2),
-	    "cont": cont
+	    "unique": uniq,
+	    "diff": diff
 	};
 }
