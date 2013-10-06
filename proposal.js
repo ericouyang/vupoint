a85bd30f66efc980d43ddee34319a2f3a864e74e
Eric Ouyang <ericouyang@gmail.com>
2013-10-06 02:15:59 -0400

    progress

--- proposal.js
+++ proposal.js
@@ -1,10 +1,12 @@
 if (Meteor.isClient) {
   Template.proposal_new.events({
     'click button' : function () {
-      Proposals.insert({
+      var id = Proposals.insert({
         project_id: Session.get('projectId'),
-        text: $("#proposal-text").val()
+        summary: $('#proposal-summary').val(),
+        text: $('#proposal-text').val(),
       });
+      Meteor.call('runNLP', id, $('#proposal-text').val());
       Meteor.Router.to('/project/'+ Session.get('projectId'));
     }
   });
--- vupoint.js
+++ vupoint.js
@@ -1,10 +1,15 @@
 Projects = new Meteor.Collection("projects");
 Proposals = new Meteor.Collection("proposals");
+Analyses = new Meteor.Collection("analyses");
+Aggregation = new Meteor.Collection("aggregations");
 
 if (Meteor.isClient) {
   Meteor.Router.add({
     '': 'home',
-    '/dashboard': 'dashboard',
+    '/dashboard': function() {
+      Meteor.subscribe("projects");
+      return 'dashboard';
+    },
     '/project/new': 'project_new',
     '/project/:id': function(id) {
       Template.project_view.project = Projects.findOne({_id: id});
@@ -58,10 +63,47 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  var alchemyKey = 'f701335d151e046d8fec89263827c5b2a7161999';
+  
   Meteor.startup(function () {
     // code to run on server at startup
   });
   
+  Meteor.methods({
+    runNLP: function (proposal_id, text) {
+      HTTP.post("http://access.alchemyapi.com/calls/text/TextGetRankedKeywords",
+        {
+          params: {
+            apikey: alchemyKey,
+            text: text,
+            sentiment: 1,
+            outputMode: 'json'
+          }
+        },
+        function (error, result) {
+          if (result.statusCode === 200) {
+            Proposals.update({_id: proposal_id}, {$set: {keywords: result.data.keywords}});
+          }
+      });
+      HTTP.post("http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities",
+        {
+          params: {
+            apikey: alchemyKey,
+            text: text,
+            sentiment: 1,
+            outputMode: 'json'
+          }
+        },
+        function (error, result) {
+          if (result.statusCode === 200) {
+            Proposals.update({_id: proposal_id}, {$set: {entities: result.data.entities}});
+            console.log(result.data.entities);
+          }
+      });
+      return 1;
+    }
+  });
+
   Meteor.publish("projects", function() {
         return Projects.find({});
     });
